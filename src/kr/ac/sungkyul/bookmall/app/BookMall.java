160be1a450f9c91cf665f37cf91600a6b6c492c9
kickscar <kickscar@gmail.com>
2016-07-28 15:50:55 +0900

    BookDao insert, getList 구현

--- src/kr/ac/sungkyul/bookmall/app/BookMall.java
+++ src/kr/ac/sungkyul/bookmall/app/BookMall.java
@@ -3,14 +3,44 @@ package kr.ac.sungkyul.bookmall.app;
 import java.util.List;
 
 import kr.ac.sungkyul.bookmall.dao.AuthorDao;
+import kr.ac.sungkyul.bookmall.dao.BookDao;
 import kr.ac.sungkyul.bookmall.vo.AuthorVo;
+import kr.ac.sungkyul.bookmall.vo.BookVo;
 
 public class BookMall {
 
 	public static void main(String[] args) {
-		testAuthorDaoInsert();
-		testAuthorDaoGetList();
+//		testAuthorDaoInsert();
+//		testAuthorDaoGetList();
 		
+		testBookDaoInsert();
+		testBookDaoGetList();
+		
+	}
+
+	public static void testBookDaoGetList(){
+		BookDao dao = new BookDao();
+		List<BookVo> list = dao.getList();
+		
+		for( BookVo vo : list ) {
+			System.out.println( vo );
+		}		
+	}
+
+	public static void testBookDaoInsert(){
+		BookDao dao = new BookDao();
+
+		BookVo vo = new BookVo();
+		vo.setTitle( "향연" );
+		vo.setRate( 1 );
+		vo.setAuthorNo( 7L );
+		dao.insert(vo);
+		
+		vo = new BookVo();
+		vo.setTitle( "맹자" );
+		vo.setRate( 1 );
+		vo.setAuthorNo( 6L );
+		dao.insert(vo);		
 	}
 
 	public static void testAuthorDaoGetList(){
